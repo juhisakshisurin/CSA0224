414ca3c27d936b3028b467302339158b4b3cd588
Juhi Surin <192525148.simats@saveetha.com>
2026-02-15 22:30:43 +0530

    C program to check if the character is a vowel or consonant

--- Vowel or Consonant.c
+++ Vowel or Consonant.c
@@ -4,12 +4,20 @@ int main()
     char ch;
     scanf(" %c",&ch);
     if (ch>='A' && ch <='Z')
+    {
         ch=ch +32;
+    }
     if (ch == 'a' || ch == 'e' || ch =='i' || ch == 'o' || ch == 'u')
+    {
         printf("%c is a Vowel\n",ch);
+    }
     else if ((ch >= 'a' && ch <= 'z'))
+    {
         printf("%c is a Consonant\n",ch);
+    }
     else
+    {
         printf("%c is not an alphabet\n",ch);
+    }
     return 0;
 }
