9a9f0c016574f10ab3493c380c7911723cf59a8a
Juhi Surin <192525148.simats@saveetha.com>
2026-02-15 22:33:16 +0530

    C program to check if number is divisible by 2 and 5

--- To check is number is divisible by 2 and 5.c
+++ To check is number is divisible by 2 and 5.c
@@ -5,8 +5,12 @@ int main()
     printf("Enter a number: ");
     scanf("%d",&n);
     if (n % 2 == 0 && n % 5 == 0)
+    {
         printf("%d is divisible by both 2 and 5\n",n);
+    }
     else
+    {
         printf("%d is not divisible by 2 and 5",n);
+    }
     return 0;
 }
