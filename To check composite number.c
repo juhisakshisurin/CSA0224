1b48fc67b7b575f5525399b5c261087b5f01d7ca
Juhi Surin <192525148.simats@saveetha.com>
2026-02-19 17:55:45 +0530

    Create To check composite number.c

---
+++ To check composite number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        printf("Not a Composite Number");
+        return 0;
+    }
+
+    for (i = 1; i <= n; i++) {
+        if (n % i == 0)
+            count++;
+    }
+
+    if (count > 2)
+        printf("%d is a Composite Number", n);
+    else
+        printf("%d is Not a Composite Number", n);
+
+    return 0;
+}
