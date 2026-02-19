36ad1fd2c0230ab635cbcd3d432346a1cb2fa6d3
Juhi Surin <192525148.simats@saveetha.com>
2026-02-19 17:44:50 +0530

    Create Print numbers from 1 to 100 except numbers divisible by both 3 and 5 using continue.c

---
+++ Print numbers from 1 to 100 except numbers divisible by both 3 and 5 using continue.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    for(int i = 1; i <= 100; i++) {
+        if(i % 3 == 0 && i % 5 == 0)
+            continue;
+        printf("%d ", i);
+    }
+    return 0;
+}
