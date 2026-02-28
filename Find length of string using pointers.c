5df7c816679b3b58a91c995f9be4a4890eac8eed
Juhi Surin <192525148.simats@saveetha.com>
2026-02-28 21:05:10 +0530

    Create Find length of string using pointers.c

---
+++ Find length of string using pointers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    char *p;
+    int len=0;
+    printf("Enter a string:");
+    scanf("%s",str);
+    p=str;
+    while (*p!='\0'){
+        len++;
+        p++;
+    }
+    printf("Length of string= %d",len);
+    return 0;
+}
