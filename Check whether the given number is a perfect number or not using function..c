763b81a8b8865577b70e4eb8fe94f4a3304d7570
Juhi Surin <192525148.simats@saveetha.com>
2026-03-02 19:16:37 +0530

    Create Check whether the given number is a perfect number or not using function..c

---
+++ Check whether the given number is a perfect number or not using function..c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int isPerfect(int n){
+    int i, sum = 0;
+    for(i = 1; i <= n/2; i++){
+        if(n % i == 0){
+            sum = sum + i;
+        }
+    }
+    if(sum == n)
+        return 1;
+    else
+        return 0;
+}
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(isPerfect(num))
+        printf("%d is a Perfect Number\n", num);
+    else
+        printf("%d is Not a Perfect Number\n", num);
+
+    return 0;
+}
