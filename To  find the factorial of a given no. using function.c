000fffedf8f51b82ed49d9cbd06c5dc6a3083470
Juhi Surin <192525148.simats@saveetha.com>
2026-03-04 06:07:18 +0530

    Create To  find the factorial of a given no. using function.c

---
+++ To  find the factorial of a given no. using function.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int factorial(int n);
+
+int main(){
+    int num, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    result = factorial(num);
+
+    printf("Factorial = %d", result);
+
+    return 0;
+}
+
+int factorial(int n){
+    int i;
+    int fact = 1;
+
+    for(i = 1; i <= n; i++){
+        fact = fact * i;
+    }
+
+    return fact;
+}
