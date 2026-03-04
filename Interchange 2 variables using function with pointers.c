f8e62477cd93264ac8d6ddaf3ebffa351d04228e
Juhi Surin <192525148.simats@saveetha.com>
2026-03-04 06:08:30 +0530

    Create Interchange 2 variables using function with pointers.c

---
+++ Interchange 2 variables using function with pointers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void swap(int *a, int *b);
+
+int main(){
+    int num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    swap(&num1, &num2);
+
+    printf("After swapping:\n");
+    printf("First number = %d\n", num1);
+    printf("Second number = %d", num2);
+
+    return 0;
+}
+
+void swap(int *a, int *b){
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
