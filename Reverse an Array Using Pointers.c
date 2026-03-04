3efcfe6ca7123c6853d793bf931a40db8f74408a
Juhi Surin <192525148.simats@saveetha.com>
2026-03-04 23:49:47 +0530

    Create Reverse an Array Using Pointers.c

---
+++ Reverse an Array Using Pointers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void reverseArray(int *arr, int n)
+{
+    int i, temp;
+
+    for(i = 0; i < n/2; i++)
+    {
+        temp = *(arr + i);
+        *(arr + i) = *(arr + n - i - 1);
+        *(arr + n - i - 1) = temp;
+    }
+}
+
+int main()
+{
+    int arr[100], n, i;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    reverseArray(arr, n);
+
+    printf("Reversed array is: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
